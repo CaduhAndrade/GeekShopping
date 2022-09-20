c85ba10ecfbc4e7e84a66b84e05219e470ca1de0
Carlos Eduardo <dev.andradecarlos@gmail.com>
2022-09-19 23:53:23 -0300

    Criação do projeto e alguns creates.

---
+++ GeekShopping.API/Config/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using GeekShopping.API.Data.ValueObjects;
+using GeekShopping.API.Model;
+
+namespace GeekShopping.API.Config
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductVO, Product>();
+                config.CreateMap<Product, ProductVO>();
+            });
+            return mappingConfig;
+        }
+    }
+}
